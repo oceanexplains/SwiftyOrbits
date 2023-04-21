90d307e90bbadb11cf4fda039251db3d0b544718
ocean aiyia paradise <121273516+oceanexplains@users.noreply.github.com>
2023-04-21 18:04:50 -0400

    Added SunView

---
+++ SwiftyOrbits/SunView.swift
@@ -0,0 +1,27 @@
+//
+//  SunView.swift
+//  SwiftyOrbits
+//
+//  Created by Tomer Zed on 4/21/23.
+//
+
+import SwiftUI
+
+
+struct SunView: View {
+    let position: CGPoint
+    
+    var body: some View {
+        Circle()
+            .fill(Color.yellow)
+            .frame(width: 15, height: 15)
+            .position(position)
+    }
+}
+
+
+struct SunView_Previews: PreviewProvider {
+    static var previews: some View {
+        SunView()
+    }
+}
---
+++ SwiftyOrbits/SwiftyOrbitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyOrbitsApp.swift
+//  SwiftyOrbits
+//
+//  Created by Tomer Zed on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyOrbitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
